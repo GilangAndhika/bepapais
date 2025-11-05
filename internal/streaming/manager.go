fd425d73fdcb1beba87d62822632ac5b25cdec71
GilangAndhika <gryzlegrizz@gmail.com>
2025-11-05 09:44:29 +0700

    Fix: Call correct ffmpeg library

--- internal/streaming/manager.go
+++ internal/streaming/manager.go
@@ -11,7 +11,7 @@ import (
 	"github.com/GilangAndhika/bepapais/internal/models"
 )
 
-const FFMPEG_PATH = "./ffmpeg" 
+const FFMPEG_PATH = "ffmpeg" 
 
 // Manager mengelola semua proses FFmpeg yang aktif
 type Manager struct {
